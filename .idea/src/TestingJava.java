1ec0cbe8c5f024b365a380fe3a99fccba78423e7
Frenguin <104327737+Frenguin@users.noreply.github.com>
2022-10-06 12:27:01 -0500

    updating testing.java

--- .idea/src/TestingJava.java
+++ .idea/src/TestingJava.java
@@ -1,5 +1,6 @@
 public class TestingJava {
     public static void main(String[] args) {
         System.out.print("hello world");
+        System.out.printf("another line");
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Alex was here
 2nd line
 3rd line
+4th line
